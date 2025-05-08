25b0069e397f8438b13187d02968a55baf333b2d
HgManuel <manuelgomezhurtado1@gmail.com>
2025-05-08 08:46:29 -0500

    math.py

--- math.py
+++ math.py
@@ -1,6 +1,6 @@
 import math
-print("Digite el numero al que desea calcularle la raiz cuadrada")
+print("Digite el numero al que desea calcularle la raiz cuarta")
 numero= float(input())
 raizC= math.sqrt(numero)
 raizC2= math.sqrt(raizC)
-print("La raiz cuadrada", numero, "es", raizC2)
+print("La raiz cuarta", numero, "es", raizC2)
